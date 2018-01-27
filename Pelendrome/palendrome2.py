fc32889e6238ac4191928e2498d4fda0aa0c2965
Sudarshan Angirash <sangirash@gmail.com>
2018-01-27 13:03:06 +0530

    removing stale entry

--- Pelendrome/palendrome2.py
+++
@@ -1,8 +0,0 @@
-import scrabble
-
-longestword = ""
-
-for word in scrabble.wordlist:
-    if list(word) == list(reversed(word)) and len(word) > len(longestword):
-        longestword = word
-print(longestword)
--- Pelendrome/palendrome3.py
+++
@@ -1,8 +0,0 @@
-import scrabble
-
-longestword = ""
-
-for word in scrabble.wordlist:
-    if word == word[::-1] and len(word) > len(longestword):
-        longestword = word
-print(longestword)
